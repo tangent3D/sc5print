5b6061e1546539504b21124524ed43cf749fa853
tangent3D <interfectum@gmail.com>
2023-09-11 17:47:51 -0700

    Successful print

--- sc5print/sc5print.c
+++ sc5print/sc5print.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-#define VRAM_SIZE 27136
+#define VRAM_SIZE 27136 // 212 rows of 128 bytes
 #define BUFFER_SIZE (VRAM_SIZE / 4) // 6784 bytes, 53 rows of 128 bytes
+
+#define EC 0x1B // PCL5 escape character
 
 int main(int argc, char* argv[])
 {
@@ -13,22 +15,22 @@ int main(int argc, char* argv[])
 
     FILE *read_ptr = fopen(argv[1], "rb"); // Attempt to load file specified by argument
 
-    if (read_ptr == NULL) // Check if file exists
+    if (read_ptr == NULL) // Verify file exists
     {
         printf("File not found.\n");
         return 1;
     }
 
-    fseek(read_ptr, 0, SEEK_END); // Check file size
+    fseek(read_ptr, 0, SEEK_END); // Verify file size
 
-    if (ftell(read_ptr) == 0) // Check if file is empty
+    if (ftell(read_ptr) == 0) // Verify file is not empty
     {
         printf("File contains no data.\n");
         fclose(read_ptr);
         return 1;
     }
 
-    if (ftell(read_ptr) != 27143) // Check if file is exactly 27,143 bytes
+    if (ftell(read_ptr) != 27143) // Verify file is exactly 27,143 bytes
     {
         fclose(read_ptr);
         printf("Invalid file size. Valid SC5 files are 27,143 bytes.\n");
@@ -41,36 +43,56 @@ int main(int argc, char* argv[])
 
     FILE *write_ptr; // Write to file for testing
     remove("out.bin"); // Get rid of existing output if exists
-    write_ptr = fopen("out.bin", "ab"); // Append to file
+    write_ptr = fopen("out.bin", "ab"); // Open and append to output file
 
-    // !!! Initialize printer for raster graphics
+    unsigned char pclInit[] =  {EC, '%', '-', '1', '2', '3', '4', '5', 'X',             // Exit language
+                                EC, 'E',                                                // Printer reset
+                                EC, '*', 'p', '3', '0', '0', 'x', '3', '0', '0', 'Y',   // Position cursor
+                                EC, '*', 't', '7', '5', 'R',                            // Raster graphics resolution
+                                EC, '*', 'r', '0', 'F',                                 // Raster presentation method
+                                EC, '*', 'r', '1', 'A'};                                // Left raster graphics margin
 
-    unsigned char byte;
+    unsigned char pclRow[] =   {EC, '*', 'b', '3', '2', 'W'};
 
-    for (int i = 0; i < (VRAM_SIZE/BUFFER_SIZE); i++) // For each chunk of file (4 chunks of 6784 bytes)
+    unsigned char pclEnd[] =   {EC, '*', 'r', 'C',
+                                EC, 'E',
+                                EC, '%', '-', '1', '2', '3', '4', '5', 'X'};
+
+    fwrite(pclInit, 1, sizeof(pclInit), write_ptr); // Write PCL initialization
+
+    for (int i = 0; i < (VRAM_SIZE / BUFFER_SIZE); i++) // For each chunk of file (4 chunks of 6784 bytes)
     {
         fread(buffer, BUFFER_SIZE, 1, read_ptr);
         for (int j = 0; j < BUFFER_SIZE; j = j + 128) // For each row in buffer (53 rows of 128 bytes)
         {
-            //fwrite(&byte, 1, 1, write_ptr);
-            // Write EC*b32W to begin each row here
-            printf("chunk %d row %d\n", i, j / 128);
-            for (int k = 0; k < 128; k = k + 4) // For each four bytes in row
+            fwrite(pclRow, 1, sizeof(pclRow), write_ptr);
+            for (int k = 0; k < 128; k = k + 4) // For each set of four VRAM bytes in row, compose one raster byte
             {
-                for (int l = 0; l < 4; l++) // For each byte in group of four bytes
+                unsigned char byte = 0;
+                unsigned char mask = 0b10000000;
+                for (int l = 0; l < 4; l++) // For each VRAM byte in set
                 {
-                    if ((buffer[j+k+l] | 0b11110000) == 0xF0)
+                    if ((buffer[j+k+l] & 0xF0) < 0xF0) // If upper nibble of byte < F, set bit
                     {
-                        printf("%d!\n", buffer[j+k+l]);
+                        byte = byte | mask;
                     }
+
+                    mask = mask >> 1; // Rotate bit mask
+
+                    if ((buffer[j+k+l] & 0x0F) < 0x0F) // If lower nibble of byte < F, set bit
+                    {
+                        byte = byte | mask;
+                    }
+
+                    mask = mask >> 1; // Rotate bit mask
+
                 }
+                fwrite(&byte, 1, 1, write_ptr); // Write composed raster byte
             }
         }
     }
 
-    // !!! close printer
-
-    // convert 128 sc5 bytes into 1 row of 32 raster bytes 212 times
+    fwrite(pclEnd, 1, sizeof(pclEnd), write_ptr);
 
     fclose(read_ptr);
     fclose(write_ptr);
